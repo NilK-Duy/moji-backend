cde070ed82eaf43d18945357ff7bd255d8af9eb2
Khanh Duy <khanh.duy.nguyen@draphony.com>
2026-03-25 19:41:57 +0700

    #1002 add friend request logic

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db';
-import authRoutes from './routes/authRoute';
-import userRoutes from './routes/userRoute';
+import authRoute from './routes/authRoute';
+import userRoute from './routes/userRoute';
+import friendRoute from './routes/friendRoute'
 import cookieParser from 'cookie-parser';
 import { protectedRoute } from './middlewares/authMiddleware';
 import cors from 'cors';
@@ -19,11 +20,12 @@ app.use(cookieParser());
 app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
 
 // public routes
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoute);
 
 // private routes
 app.use(protectedRoute)
-app.use('/api/users', userRoutes);
+app.use('/api/users', userRoute);
+app.use('/api/friends', friendRoute)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
